e19bf407b82aa347f3daaef5678ccae9c269c373
Sharukh Hasan <sharukh-hasan@uiowa.edu>
2016-02-21 06:04:06 -0600

    added doc view activity

---
+++ app/src/main/java/com/sharukhhasan/docupload/activities/DocumentViewActivity.java
@@ -0,0 +1,13 @@
+package com.sharukhhasan.docupload.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DocumentViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_document_view);
+    }
+}
