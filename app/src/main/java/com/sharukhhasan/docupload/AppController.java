bae6d296a99c47d0f5e55a9c6f9f4a74ad406e30
Sharukh Hasan <sharukh-hasan@uiowa.edu>
2016-02-21 05:20:39 -0600

    Added splash activity and app controller

---
+++ app/src/main/java/com/sharukhhasan/docupload/AppController.java
@@ -0,0 +1,7 @@
+package com.sharukhhasan.docupload;
+
+/**
+ * Created by Sharukh on 2/21/16.
+ */
+public class AppController {
+}
---
+++ app/src/main/java/com/sharukhhasan/docupload/activities/SplashActivity.java
@@ -0,0 +1,63 @@
+package com.sharukhhasan.docupload.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+
+import com.sharukhhasan.docupload.R;
+
+import com.parse.ParseUser;
+import com.parse.ParseAnonymousUtils;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread timerThread = new Thread(){
+            public void run()
+            {
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    ParseUser currentUser = ParseUser.getCurrentUser();
+                    if(ParseAnonymousUtils.isLinked(ParseUser.getCurrentUser()) || currentUser == null)
+                    {
+                        Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                    else
+                    {
+                        if(ParseUser.getCurrentUser() != null)
+                        {
+                            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                        else
+                        {
+                            Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        finish();
+    }
+
+}
