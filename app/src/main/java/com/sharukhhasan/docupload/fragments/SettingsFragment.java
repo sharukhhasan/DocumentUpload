2ef04c4b4532c29f735e70629b0d10f5bc9ba692
Sharukh Hasan <sharukh-hasan@uiowa.edu>
2016-02-23 13:20:27 -0600

    added settings fragment

--- app/src/main/java/com/sharukhhasan/docupload/fragments/SettingsFragment.java
+++ app/src/main/java/com/sharukhhasan/docupload/fragments/SettingsFragment.java
@@ -1,7 +1,26 @@
 package com.sharukhhasan.docupload.fragments;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by Sharukh on 2/22/16.
  */
-public class SettingsFragment {
+public class SettingsFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflator, ViewGroup view, Bundle savedInstanceState)
+    {
+        return null;
+    }
 }
